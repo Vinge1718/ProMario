12a8b65ace41414f7b52fd63816e151d4e430608
Vinge1718 <vinge1718@gmail.com>
2018-07-11 03:01:37 +0300

    Initial commit

---
+++ desktop/src/io/github/vinge1718/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.vinge1718.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.vinge1718.MyProgrammingMario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyProgrammingMario(), config);
+	}
+}
