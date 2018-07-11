134ba5ab80e53fa0a88d37309c6a6128a32adb2e
Vinge1718 <vinge1718@gmail.com>
2018-07-11 10:32:14 +0300

    added the tiled map, the tiled map loader and the Orthogonaltiled MapRender that renders the widgets that are in the tmx file

--- core/src/io/github/vinge1718/Screens/PlayScreen.java
+++ core/src/io/github/vinge1718/Screens/PlayScreen.java
@@ -5,24 +5,39 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import java.util.logging.Level;
+
 import io.github.vinge1718.MyProgrammingMario;
+import io.github.vinge1718.Scenes.Hud;
 
 public class PlayScreen implements Screen {
     private  MyProgrammingMario game;
     private OrthographicCamera gamecam;
     private Viewport gamePort;
+    Hud hud;
 
-    Texture texture;
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public PlayScreen (MyProgrammingMario game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");
         gamecam = new OrthographicCamera();
         gamePort = new FitViewport(MyProgrammingMario.V_WIDTH, MyProgrammingMario.V_HEIGHT, gamecam);
+        hud = new Hud(game.batch);
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gamePort.getWorldWidth()/2, gamePort.getWorldHeight()/2, 0);
+
     }
 
     @Override
@@ -30,19 +45,31 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched())
+            gamecam.position.x += 100 *dt;
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,0,0,1);
+        update(delta);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(gamecam.combined);
-        game.batch.begin();
-        game.batch.draw(texture, 0,0);
-        game.batch.end();
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+        renderer.render();
     }
 
     @Override
     public void resize(int width, int height) {
         gamePort.update(width, height);
+
     }
 
     @Override
