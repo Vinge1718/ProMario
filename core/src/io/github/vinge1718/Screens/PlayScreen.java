9edda522ab25750e7c8f9160820cb0941ea0dac4
Vinge1718 <vinge1718@gmail.com>
2018-07-11 03:28:33 +0300

    added the playscreen class to handle the rendering and screen clearing

--- core/src/io/github/vinge1718/Screens/PlayScreen.java
+++ core/src/io/github/vinge1718/Screens/PlayScreen.java
@@ -1,4 +1,56 @@
 package io.github.vinge1718.Screens;
 
-public class PlayScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+import io.github.vinge1718.MyProgrammingMario;
+
+public class PlayScreen implements Screen {
+    private  MyProgrammingMario game;
+    Texture texture;
+    public PlayScreen (MyProgrammingMario game){
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture, 0,0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
