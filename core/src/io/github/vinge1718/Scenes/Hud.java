7efd174a43b23b1f54f4091ac47f12a9b9ba8042
Vinge1718 <vinge1718@gmail.com>
2018-07-11 04:15:09 +0300

    created the constants and variables for the stage where our widgets will layed out and organised

--- core/src/io/github/vinge1718/Scenes/Hud.java
+++ core/src/io/github/vinge1718/Scenes/Hud.java
@@ -1,4 +1,44 @@
 package io.github.vinge1718.Scenes;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.awt.Label;
+
+import io.github.vinge1718.MyProgrammingMario;
+import sun.util.resources.ms.CalendarData_ms_MY;
+
 public class Hud {
+    private Stage stage;
+    private Viewport viewport;
+
+    private Integer worldTimer;
+    private float timeCount;
+    private Integer score;
+
+    Label countDownLabel;
+    Label scoreLabel;
+    Label timeLabel;
+    Label levelLabel;
+    Label worldLabel;
+    Label marioLabel;
+
+    public Hud(SpriteBatch sb){
+        worldTimer = 300;
+        timeCount = 0;
+        score = 0 ;
+
+        viewport = new FitViewport(MyProgrammingMario.V_WIDTH, MyProgrammingMario.V_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewport, sb);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+
+    }
 }
