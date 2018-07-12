0612ba9b13fe93fda23e8ad4e84a6824ea89ccfe
Vinge1718 <vinge1718@gmail.com>
2018-07-12 10:39:25 +0300

    changed the mario constructor to inlcude the updated screen

--- core/src/io/github/vinge1718/Sprites/Mario.java
+++ core/src/io/github/vinge1718/Sprites/Mario.java
@@ -1,6 +1,8 @@
 package io.github.vinge1718.Sprites;
 
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -8,14 +10,20 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 import io.github.vinge1718.MyProgrammingMario;
+import io.github.vinge1718.Screens.PlayScreen;
 
 public class Mario extends Sprite {
     public World world;
     public Body b2body;
+    private TextureRegion  marioStand;
 
-    public Mario(World world){
+    public Mario(World world, PlayScreen screen){
+        super(screen.getAtlas().findRegion("little_mario"));
         this.world = world;
         defineMario();
+        marioStand = new TextureRegion(getTexture(), 0, 0, 16, 16);
+        setBounds(0,0,16/MyProgrammingMario.PPM, 16/MyProgrammingMario.PPM);
+        setRegion(marioStand);
     }
 
     public void defineMario(){
