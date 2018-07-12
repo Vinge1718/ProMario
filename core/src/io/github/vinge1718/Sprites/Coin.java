210688a45d1a2a7bb53f8d7a76bf3350f441cd47
Vinge1718 <vinge1718@gmail.com>
2018-07-13 00:03:18 +0300

    innitialize a variable to fetch the tile/icon to act as a replacement to the tile/texure/icon/widget hit by marios head

--- core/src/io/github/vinge1718/Sprites/Coin.java
+++ core/src/io/github/vinge1718/Sprites/Coin.java
@@ -2,6 +2,8 @@ package io.github.vinge1718.Sprites;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
@@ -11,8 +13,11 @@ import com.badlogic.gdx.physics.box2d.World;
 import io.github.vinge1718.MyProgrammingMario;
 
 public class Coin extends InteractiveTileObject {
+    private static TiledMapTileSet tileSet;
+
     public Coin(World world, TiledMap map, Rectangle bounds){
         super(world, map, bounds);
+        tileSet = map.getTileSets().getTileSet("tileset_gutter");
         fixture.setUserData(this);
         setCategoryFilter(MyProgrammingMario.COIN_BIT);
     }
