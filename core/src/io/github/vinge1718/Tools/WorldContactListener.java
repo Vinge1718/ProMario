436f299714dcbd643a0f93efc972a55f37f75e3f
Vinge1718 <vinge1718@gmail.com>
2018-07-12 20:04:45 +0300

    create a function that will single out contact between marios head and other active components of the game

--- core/src/io/github/vinge1718/Tools/WorldContactListener.java
+++ core/src/io/github/vinge1718/Tools/WorldContactListener.java
@@ -4,15 +4,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 public class WorldContactListener implements ContactListener {
 
     @Override
     public void beginContact(Contact contact) {
-        Gdx.app.log("Begin Contact", "");
+        Fixture fixA = contact.getFixtureA();
+        Fixture fixB = contact.getFixtureB();
+
+        if(fixA.getUserData() == "head" || fixB.getUserData() == "head"){
+            Fixture head = fixA.getUserData() == "head" ? fixA : fixB;
+            Fixture object = head == fixA ? fixB : fixA;
+        }
     }
 
     @Override
