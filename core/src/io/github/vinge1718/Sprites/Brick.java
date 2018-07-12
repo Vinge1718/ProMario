2a9022f5573b591a63ec52367430587d21f9be64
Vinge1718 <vinge1718@gmail.com>
2018-07-12 21:00:34 +0300

    fetch the tile that mario hits with his head and set it to null

--- core/src/io/github/vinge1718/Sprites/Brick.java
+++ core/src/io/github/vinge1718/Sprites/Brick.java
@@ -1,18 +1,24 @@
 package io.github.vinge1718.Sprites;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
+
+import io.github.vinge1718.MyProgrammingMario;
 
 public class Brick extends InteractiveTileObject{
     public Brick(World world, TiledMap map, Rectangle bounds){
         super(world, map, bounds);
         fixture.setUserData(this);
+        setCategoryFilter(MyProgrammingMario.BRICK_BIT);
     }
 
     @Override
     public void onHeadHit() {
         Gdx.app.log("Brick", "Collision");
+        setCategoryFilter(MyProgrammingMario.DESTROYED_BIT);
+        getCell().setTile(null);
     }
 }
