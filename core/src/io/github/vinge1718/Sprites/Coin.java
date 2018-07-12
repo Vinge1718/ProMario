731f89999006cbb55ca1ded8b4d04a48627a7db6
Vinge1718 <vinge1718@gmail.com>
2018-07-12 18:56:37 +0300

    log its own name to the console upon collision with the mario texture - just like the coin object (as of now)

--- core/src/io/github/vinge1718/Sprites/Coin.java
+++ core/src/io/github/vinge1718/Sprites/Coin.java
@@ -1,5 +1,6 @@
 package io.github.vinge1718.Sprites;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -13,5 +14,10 @@ public class Coin extends InteractiveTileObject {
     public Coin(World world, TiledMap map, Rectangle bounds){
         super(world, map, bounds);
         fixture.setUserData(this);
+    }
+
+    @Override
+    public void onHeadHit() {
+        Gdx.app.log("Coin", "Collision");
     }
 }
