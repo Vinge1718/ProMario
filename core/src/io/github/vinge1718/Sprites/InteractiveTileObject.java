d1929b5f16fb79fac2d34f40b8aa311650ee1d27
Vinge1718 <vinge1718@gmail.com>
2018-07-12 16:12:18 +0300

    captured the part of the texture that can editted.

--- core/src/io/github/vinge1718/Sprites/InteractiveTileObject.java
+++ core/src/io/github/vinge1718/Sprites/InteractiveTileObject.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
@@ -17,6 +18,7 @@ public class InteractiveTileObject {
     protected TiledMapTile tile;
     protected Rectangle bounds;
     protected Body body;
+    protected Fixture fixture;
 
     public InteractiveTileObject(World world, TiledMap map, Rectangle bounds){
         this.world = world;
@@ -34,6 +36,6 @@ public class InteractiveTileObject {
 
         shape.setAsBox(bounds.getWidth()/2/MyProgrammingMario.PPM, bounds.getHeight()/2/MyProgrammingMario.PPM);
         fdef.shape = shape;
-        body.createFixture(fdef);
+        fixture = body.createFixture(fdef);
     }
 }
