e94daefda1e7484dee4c229d17c486d8c4fd1d21
Vinge1718 <vinge1718@gmail.com>
2018-07-14 12:23:54 +0300

    fetching the bits/data if mario jumps and/or simply collides with the goomba. In one case the bits will represent death of mario and in the other case the death of the goomba

--- core/src/io/github/vinge1718/Tools/WorldContactListener.java
+++ core/src/io/github/vinge1718/Tools/WorldContactListener.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
+import io.github.vinge1718.MyProgrammingMario;
+import io.github.vinge1718.Sprites.Enemy;
 import io.github.vinge1718.Sprites.InteractiveTileObject;
 
 
@@ -17,6 +19,8 @@ public class WorldContactListener implements ContactListener {
         Fixture fixA = contact.getFixtureA();
         Fixture fixB = contact.getFixtureB();
 
+        int cDef = fixA.getFilterData().categoryBits | fixB.getFilterData().categoryBits;
+
         if(fixA.getUserData() == "head" || fixB.getUserData() == "head"){
             Fixture head = fixA.getUserData() == "head" ? fixA : fixB;
             Fixture object = head == fixA ? fixB : fixA;
@@ -25,6 +29,15 @@ public class WorldContactListener implements ContactListener {
                 ((InteractiveTileObject) object.getUserData()).onHeadHit();
             }
         }
+
+        switch (cDef){
+            case MyProgrammingMario.ENEMY_HEAD_BIT | MyProgrammingMario.MARIO_BIT:
+                if(fixA.getFilterData().categoryBits == MyProgrammingMario.ENEMY_HEAD_BIT)
+                    ((Enemy)fixA.getUserData()).hitOnHead();
+                else if(fixB.getFilterData().categoryBits == MyProgrammingMario.ENEMY_HEAD_BIT)
+                    ((Enemy)fixB.getUserData()).hitOnHead();
+        }
+
     }
 
     @Override
