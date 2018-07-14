aa7d36eaa49f563444ce5e83dce954154d836f21
Vinge1718 <vinge1718@gmail.com>
2018-07-14 08:56:10 +0300

    remove the run-time error by removing the second disposal of the spritebatch

--- core/src/io/github/vinge1718/MyProgrammingMario.java
+++ core/src/io/github/vinge1718/MyProgrammingMario.java
@@ -21,7 +21,11 @@ public class MyProgrammingMario extends Game {
 	public static final short BRICK_BIT = 4;
 	public static final short COIN_BIT =8;
 	public static final short DESTROYED_BIT = 16;
-	public static final short DEFAULT_BIT = 1;
+	public static final short GROUND_BIT = 1;
+	public static final short OBJECT_BIT =32;
+	public static final short ENEMY_BIT = 64;
+    public static final short ENEMY_HEAD_BIT = 128;
+
 	public static AssetManager manager;
 
 	public SpriteBatch batch;
