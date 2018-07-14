48d03a463892c5166f0d9e906a860719e20fcb75
Vinge1718 <vinge1718@gmail.com>
2018-07-14 10:05:58 +0300

    induct the screen play in the constructor in order to be part of the gameplay

--- core/src/io/github/vinge1718/Sprites/Enemy.java
+++ core/src/io/github/vinge1718/Sprites/Enemy.java
@@ -1,4 +1,22 @@
 package io.github.vinge1718.Sprites;
 
-public class Enemy {
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+
+import io.github.vinge1718.Screens.PlayScreen;
+
+public abstract class Enemy extends Sprite {
+    protected PlayScreen screen;
+    protected World world;
+    protected Body b2body;
+
+    public Enemy(PlayScreen screen, float x, float y){
+        this.world = screen.getWorld();
+        this.screen = screen;
+        setPosition(x,y);
+        definedEnemy();
+    }
+
+    protected abstract void definedEnemy();
 }
