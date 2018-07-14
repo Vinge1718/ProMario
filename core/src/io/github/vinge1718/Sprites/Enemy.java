3be6246ff6f480b36c69442edb2df30feca08358
Vinge1718 <vinge1718@gmail.com>
2018-07-14 13:04:43 +0300

    intantiated a new vector quantity to facilitate goombas lateral motion. Then define it to only affet the X-axis of motion

--- core/src/io/github/vinge1718/Sprites/Enemy.java
+++ core/src/io/github/vinge1718/Sprites/Enemy.java
@@ -1,6 +1,7 @@
 package io.github.vinge1718.Sprites;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -10,14 +11,23 @@ public abstract class Enemy extends Sprite {
     protected PlayScreen screen;
     protected World world;
     protected Body b2body;
+    public Vector2 velocity;
 
     public Enemy(PlayScreen screen, float x, float y){
         this.world = screen.getWorld();
         this.screen = screen;
         setPosition(x,y);
         definedEnemy();
+        velocity =new Vector2(1,0);
     }
 
     protected abstract void definedEnemy();
     public abstract void hitOnHead();
+
+    public void reverseVelocity(boolean x, boolean y){
+        if(x)
+            velocity.x = -velocity.x;
+        if(y)
+            velocity.y = velocity.y;
+    }
 }
