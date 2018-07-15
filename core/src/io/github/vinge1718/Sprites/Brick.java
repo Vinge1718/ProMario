754e7149f5f8d9628fe95962cf63d77faa6e2169
Vinge1718 <vinge1718@gmail.com>
2018-07-16 00:34:18 +0300

    replaced bounds with objects as reflected in the main class from which the brick inherits

--- core/src/io/github/vinge1718/Sprites/Brick.java
+++ core/src/io/github/vinge1718/Sprites/Brick.java
@@ -1,6 +1,7 @@
 package io.github.vinge1718.Sprites;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
@@ -11,8 +12,8 @@ import io.github.vinge1718.Scenes.Hud;
 import io.github.vinge1718.Screens.PlayScreen;
 
 public class Brick extends InteractiveTileObject{
-    public Brick(PlayScreen screen, Rectangle bounds){
-        super(screen, bounds);
+    public Brick(PlayScreen screen, MapObject object){
+        super(screen, object);
         fixture.setUserData(this);
         setCategoryFilter(MyProgrammingMario.BRICK_BIT);
     }
@@ -23,6 +24,6 @@ public class Brick extends InteractiveTileObject{
         setCategoryFilter(MyProgrammingMario.DESTROYED_BIT);
         getCell().setTile(null);
         Hud.addScore(200);
-//        MyProgrammingMario.manager.get("audio/sounds/breakblock.wav");
+        MyProgrammingMario.manager.get("audio/sounds/breakblock.wav", Sound.class).play();
     }
 }
