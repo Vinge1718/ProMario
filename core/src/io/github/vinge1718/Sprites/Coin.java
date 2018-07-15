dce61100da61fb0ed323d42fabec2d57dde0467d
Vinge1718 <vinge1718@gmail.com>
2018-07-16 00:40:30 +0300

    added the ability of the coin spawning tiles to spawn mushroom and develop their own custom sounds

--- core/src/io/github/vinge1718/Sprites/Coin.java
+++ core/src/io/github/vinge1718/Sprites/Coin.java
@@ -2,10 +2,12 @@ package io.github.vinge1718.Sprites;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
@@ -19,8 +21,8 @@ public class Coin extends InteractiveTileObject {
     private static TiledMapTileSet tileSet;
     private final int BLANK_COIN = 28;
 
-    public Coin(PlayScreen screen, Rectangle bounds){
-        super(screen, bounds);
+    public Coin(PlayScreen screen, MapObject object){
+        super(screen, object);
         tileSet = map.getTileSets().getTileSet("tileset_gutter");
         fixture.setUserData(this);
         setCategoryFilter(MyProgrammingMario.COIN_BIT);
@@ -29,10 +31,17 @@ public class Coin extends InteractiveTileObject {
     @Override
     public void onHeadHit() {
         Gdx.app.log("Coin", "Collision");
-//        if(getCell().getTile().getId() == BLANK_COIN)
-//            MyProgrammingMario.manager.get("audio/sounds/bump.wav", Sound.class).play();
-//        else
-//            MyProgrammingMario.manager.get("audio/sounds/coin.wav", Sound.class).play();
+        if(getCell().getTile().getId() == BLANK_COIN)
+            MyProgrammingMario.manager.get("audio/sounds/bump.wav", Sound.class).play();
+        else{
+            if(object.getProperties().containsKey("mushroom")) {
+                screen.spawnItem(new ItemDef(new Vector2(body.getPosition().x, body.getPosition().y + 16 / MyProgrammingMario.PPM), Mushroom.class));
+                MyProgrammingMario.manager.get("audio/sounds/powerup_spawn.wav", Sound.class).play();
+            }
+            else
+            MyProgrammingMario.manager.get("audio/sounds/coin.wav", Sound.class).play();
+        }
+
         getCell().setTile(tileSet.getTile(BLANK_COIN));
         Hud.addScore(100);
     }
