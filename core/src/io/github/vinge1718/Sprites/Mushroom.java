b414f2d286308c6c677059392076a8764d47e365
Vinge1718 <vinge1718@gmail.com>
2018-07-16 01:03:16 +0300

    add the objects with which the mushroom can collide with as well as define the repurcusions

--- core/src/io/github/vinge1718/Sprites/Mushroom.java
+++ core/src/io/github/vinge1718/Sprites/Mushroom.java
@@ -1,4 +1,51 @@
 package io.github.vinge1718.Sprites;
 
-public class Mushroom {
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+import io.github.vinge1718.MyProgrammingMario;
+import io.github.vinge1718.Screens.PlayScreen;
+
+public class Mushroom extends Item{
+    public Mushroom(PlayScreen screen, float x, float y) {
+        super(screen, x, y);
+        setRegion(screen.getAtlas().findRegion("mushroom"), 0, 0, 16, 16);
+        velocity = new Vector2(0.7f, 0);
+    }
+
+    @Override
+    public void defineItem() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(getX() , getY());
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(6/ MyProgrammingMario.PPM);
+        fdef.filter.categoryBits = MyProgrammingMario.ITEM_BIT;
+        fdef.filter.maskBits = MyProgrammingMario.MARIO_BIT |
+                MyProgrammingMario.GROUND_BIT |
+                MyProgrammingMario.COIN_BIT |
+                MyProgrammingMario.BRICK_BIT |
+                MyProgrammingMario.OBJECT_BIT ;
+
+        fdef.shape = shape;
+        body.createFixture(fdef).setUserData(this);
+    }
+
+    @Override
+    public void use(Mario mario ) {
+        destroy();
+    }
+
+    @Override
+    public void update(float dt) {
+        super.update(dt);
+        setPosition(body.getPosition().x - getWidth() /2, body.getPosition().y - getHeight()/2);
+        velocity.y = body.getLinearVelocity().y;
+        body.setLinearVelocity(velocity);
+    }
 }
