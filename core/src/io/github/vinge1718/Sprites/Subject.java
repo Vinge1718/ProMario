0d239ead04b4b230e90b729d0a2adda175aa2ad5
victor king'e <vinge1718@gmail.com>
2018-08-02 07:35:31 +0300

    added the subject class meant to hold the various different subject content according to the content published

---
+++ core/src/io/github/vinge1718/Sprites/Subject.java
@@ -0,0 +1,24 @@
+package io.github.vinge1718.Sprites;
+
+import io.github.vinge1718.Screens.PlayScreen;
+
+public class Subject extends Item {
+    public Subject(PlayScreen screen, float x, float y) {
+        super(screen, x, y);
+    }
+
+    @Override
+    public void defineItem() {
+
+    }
+
+    @Override
+    public void use(Mario mario) {
+
+    }
+
+    @Override
+    public void update(float dt) {
+        super.update(dt);
+    }
+}
